037e3ab1c5ba054a03cb2c10ba7a3aff12c4a72d
k.mekhedenko <k.mekhedenko@gmail.com>
2024-09-05 21:33:11 +0200

    linting is so much fun

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -123,14 +123,15 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
             post_review(data)
             return JsonResponse({"status": 200})
         except Exception as err:
             print(f"Unexpected {err=}, {type(err)=}")
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse({"status": 401,
+                                 "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 
